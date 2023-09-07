34beda991b3298dbb82d75c45edb563b02999827
andreasdelu <andreasdelu@gmail.com>
2023-09-07 10:33:36 +0200

    removed function call from index

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const {
 } = require("./functions");
 const { generateJWTSecret } = require("./utils");
 
-hashPassword("password").then((hash) => {
-	console.log(hash);
-});
-
 module.exports = {
 	authenticate,
 	ensureAuth,
